7615c6016b87260c2229458e404250910c8a9dd1
mattyshalom <olusegesunday@yahoo.com>
2022-10-26 18:36:16 +0100

    Quest06

---
+++ js_Quest06/ex03/my_size.js
@@ -0,0 +1,16 @@
+/*
+**
+** QWASAR.IO -- my_size
+**
+**
+** @param {String} param_1
+** @return {integer} size of the string
+
+**
+*/
+
+
+function my_size(param_1) {
+    return param_1.length;
+
+};
